5bc881203f016e1fe08e75c3bc57b90c324393b6
iamtrazy <73080739+iamtrazy@users.noreply.github.com>
2021-05-02 10:27:17 +0530

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -1,34 +1,24 @@
 #!/bin/sh
 
-#change localtime
-#rm -rf /etc/localtime
-#cp /usr/share/zoneinfo/Asia/Colombo /etc/localtime
-
 # config xray
 cat << EOF > /etc/config.json
 {
   "inbounds":[
-        {
-            "port": $PORT,
-            "protocol": "$PROTOCOL",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "$UUID", 
-                        "level": 0,
-                        "email": "love@example.com"
-                    }
-                ],
-                "decryption": "none"
-            },
-            "streamSettings": {
-                "network": "ws",
-                "security": "none",
-                "wsSettings": { 
-                    "path": "/websocket"
-                }
-            }
-        }
+    {
+      "port": $PORT,
+      "protocol": "$PROTOCOL",
+      "settings": {
+        "decryption": "none",
+        "clients": [
+          {
+            "id": "$UUID"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    }
   ],
   "outbounds": [
     {
