cdf318a648e2ee8ce028c37c4de9afea1905f1c6
iamtrazy <73080739+iamtrazy@users.noreply.github.com>
2021-05-02 10:31:04 +0530

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -4,21 +4,27 @@
 cat << EOF > /etc/config.json
 {
   "inbounds":[
-    {
-      "port": $PORT,
-      "protocol": "$PROTOCOL",
-      "settings": {
-        "decryption": "none",
-        "clients": [
-          {
-            "id": "$UUID"
-          }
-        ]
-      },
-      "streamSettings": {
-        "network": "ws"
-      }
-    }
+        {
+            "port": $PORT,
+            "protocol": "$PROTOCOL",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID", 
+                        "level": 0,
+                        "email": "love@example.com"
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": { 
+                    "path": "/websocket"
+                }
+            }
+        }
   ],
   "outbounds": [
     {
